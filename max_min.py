04a0354ee5d2f46ec9c22e623159d79ec10a67b4
fredrickochieng <fredrickochieng2013@gmail.com>
2017-01-18 12:39:51 +0300

    a function to return minimum and a maximum values of an array

---
+++ max_min.py
@@ -0,0 +1,12 @@
+def find_max_min(list):
+	maximum=max(list)
+	minimum=min(list)
+	if maximum !=minimum:
+		return [minimum,maximum]
+	else:
+		return [minimum]
+    
+               
+    	   
+
+		
